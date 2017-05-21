ba0c2241f7bd09390689a6c3c5ec00a8fc715958
Scottishdwarf <cloudmagic@protonmail.ch>
2017-05-21 21:40:25 +0300

    Update Druid-Balance-Scotishdwarf.cs

--- Rotations/Druid/Druid-Balance-Scotishdwarf.cs
+++ Rotations/Druid/Druid-Balance-Scotishdwarf.cs
@@ -2197,7 +2197,7 @@ namespace CloudMagic.Rotation
 [AddonDetails.db]
 AddonAuthor=Daniel and Scotishdwarf
 AddonName=HideOrderHallBar
-WoWVersion=Legion - 70100
+WoWVersion=Legion - 72000
 [SpellBook.db]
 Spell,8921,Moonfire,D4
 Spell,93402,Sunfire,F8

401169d036f6a2896fc1144b5ce3f2ebff82a393
Vectarius <capcommueller@freenet.de>
2017-05-29 17:29:44 +0200

    Update Hunter-Beast Mastery-Vectarius.cs

--- Rotations/Hunter/Hunter-Beast Mastery-Vectarius.cs
+++ Rotations/Hunter/Hunter-Beast Mastery-Vectarius.cs
@@ -728,14 +728,14 @@ SettingsForm = new Form {Text = "Beast Mastery Hunter", StartPosition = FormStar
                 if ((WoW.CanCast("Healthstone") || WoW.CanCast("Potion"))
                     && (WoW.ItemCount("Healthstone") >= 1 || WoW.ItemCount("Potion") >= 1)
                     && (!WoW.ItemOnCooldown("Healthstone") || !WoW.ItemOnCooldown("Potion"))
-                    && WoW.HealthPercent <= ConfigFile.ReadValue<int>("Hunter", "Potion Percent") && WoW.HealthPercent != 0)
+                    && WoW.HealthPercent <= ConfigFile.ReadValue<int>("Hunter", "Potion Percent") && WoW.HealthPercent != 0 && Potion)
                 {
                     Thread.Sleep(500);
                     WoW.CastSpell("Healthstone");
                     WoW.CastSpell("Potion");
                     return;
                 }					
-                    if (BL && WoW.CanCast("Pot") && !WoW.PlayerHasBuff("Pot") && WoW.ItemCount("Pot") >= 1 && !WoW.ItemOnCooldown("Pot"))
+                    if (Pot && BL && WoW.CanCast("Pot") && !WoW.PlayerHasBuff("Pot") && WoW.ItemCount("Pot") >= 1 && !WoW.ItemOnCooldown("Pot"))
 					{
 						WoW.CastSpell("Pot") ;
 						return;
